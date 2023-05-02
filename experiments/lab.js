805654bc62e757d1a0b7c542aaa955e140672286
paulinaepatino <ppatino@ucsc.edu>
2023-05-02 11:28:30 -0700

    lab7

--- experiments/lab.js
+++ experiments/lab.js
@@ -1,10 +1,5 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
-
-// Constants
-
-// Functions
+// JavaScript Experiments & Notes
+//    notes following lectures
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
@@ -40,3 +35,38 @@ myMainRide = {
 }
 
 console.log("my dream ride is a", myMainRide.color, myMainRide.year, myMainRide.make, myMainRide.model, "I would put a cute bumper sticker on it")
+
+// lab 7 notes - functions
+// 5/2/23
+
+function multiply(x, y,) {
+  var product = x * y;
+  return product;
+}
+
+var z = multiply(256, 256)
+
+
+
+function myTransport(bus, walk, plane, uber)
+
+var sortedArray = array.sort();
+var reversedArray = sortedArray.reverse();
+
+sortedArray([1, 2, 13, 5])
+
+// Write a JavaScript function that accepts an array and sorts it in descending order.
+var myTransport = ["bus", "walk", "plane", "uber"]
+  .sort(myTransport)
+  .reverse (myTRansport)
+
+// Have the function return the new array
+
+// Define an array of numbers.
+a = [1, 2, 4, 13, 34, 5]
+
+// Call the function, passing the array, and print the results to the console.
+
+
+
+
---
+++ lab7/js/lab.js
@@ -0,0 +1,55 @@
+// index.js - Lab 7 - Functions
+// Author: Paulina Patino & Sa'Niya Murphy
+// Date: 5/4/23
+
+
+// Put a comment block at the top of your program
+
+// Create a new function in your file, call it what you like, but choose wisely.
+
+// In the function, declare a variable userName and use window.prompt() to get the user's name from the user. Here's some help.
+main();
+function reverseString(person){
+  return person.split('').reverse().join('');
+}
+
+// Make sure your function uses return to return the results.
+function reversePerson(person){
+  return person.split('').reverse().join('');
+}
+
+function fixPerson(person){
+  document.writeln("Let me fix your name: ",
+        fixPerson(person),"</br>",
+        JSON.stringify(person, null, '\t'), "</pre>");
+  }
+
+function main(){
+let person = prompt("Please enter your name",);
+
+  "Hello " + person + "! How are you today?";
+
+
+// Sort the letters of the user's name and return those from the function.
+  // Hint: You can't sort strings, but you can sort arrays. Can you convert it back and forth?
+
+  
+reverseString(person);
+document.writeln(reverseString(person))
+
+
+
+
+
+  
+     
+   
+// Outside of the function, call the function and output the results with document.writeln() to neatly output the user's sorted name.
+
+  console.log("Main function started.");
+ 
+//output
+document.writeln("fixing your name..." + person + "<br>")
+
+console.log("what a beautiful name " + person + "is!")
+}
